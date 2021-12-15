b314c23d833b01e18717746162dc403120101445
Kyle J Sherman <64098662+kylejsherman@users.noreply.github.com>
2021-12-14 19:09:25 -0500

    first commit!

---
+++ geodeHomestead/terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "/Users/kyle/.aws/credentials"
+  profile                 = "default"
+  allowed_account_ids = ["595055244566"]
+  default_tags {
+    tags = {
+      Tree = "Bristlecone"
+      Water = "MantaRay"
+      Earth = "Moose"
+      Air = "ShoebillStork"
+      ManagedBy = "terraform"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "static_webpage" {
+  bucket = var.website_bucket_name
+  acl = "public-read"
+  policy = templatefile(
+    "${path.module}/policies/s3_public_read.json.tpl",
+    {
+      bucket_name = var.website_bucket_name
+    }
+    )
+
+  website {
+    index_document = "index.html"
+  }
+}
